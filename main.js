cc86c81d00af1d049ec96ce5473fd911303a68e8
rominavizzuett <131417017+rominavizzuett@users.noreply.github.com>
2024-01-29 18:00:38 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("Mi primer proyecto en la consola");
+
+            var primer_nombre = "Romina";
+            var apellido = "Vizzuett";
+
+            var nombre_completo = primer_nombre.concat(apellido);
+            console.log(nombre_completo);
+
+            var a = 26;
+            var b = 18;
+
+            var sumar = a + b ;
+            console.log(sumar);
+
+            var restar = a - b ;
+            console.log(restar);
+
+            var multiplicar = a * b ;
+            console.log(multiplicar);
+
+            var dividir = a / b ;
+            console.log(dividir);
